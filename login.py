6e0f8e005d281c4f8f0ca0c6c8f1c4e037930c4a
qiu <ql123ql123@163.com>
2019-12-09 15:23:20 +0800

    售票界面V0.0

--- login.py
+++ login.py
@@ -5,16 +5,17 @@ from LoginUI import Ui_Form
 from ConductorUI import Ui_Dialog
 
 class Login(QtWidgets.QWidget, Ui_Form):
-    def __init__(self):
+    def __init__(self, conductorui):
         super(Login, self).__init__()
         self.setupUi(self)
+        self.conductorui = conductorui
 
     def accept(self):
         if(self.ifconductor.isChecked()):
             self.conn = psycopg2.connect(database="TicketManagementSystem", user=self.nametext.toPlainText(),
                                          password=self.passwordtext.toPlainText(), host="localhost", port="5432")
-            conductorui = Conductor(self.conn)
-            conductorui.show()
+            self.conductorui.connectDB(self.conn)
+            self.conductorui.show()
             self.close()
         elif(self.ifmanager.isChecked()):
             self.conn = psycopg2.connect(database="TicketManagementSystem", user=self.nametext.toPlainText(),
@@ -28,17 +29,26 @@ class Login(QtWidgets.QWidget, Ui_Form):
         sys.exit(app.exec())
 
 class Conductor(QtWidgets.QDialog, Ui_Dialog):
-    def __init__(self, conn):
+    def __init__(self):
         super(Conductor, self).__init__()
         self.setupUi(self)
-        self.conn = conn
 
-    def find(self):
-        cur = self.conn.cursor()
-        cur.execute()
+    def connectDB(self,conn):
+        self.conn = conn
+        self.cur = self.conn.cursor()
+
+        #self.cur.execute("select s_sname from station;")
+        #slist = self.cur.fetchall()
+        #print(slist)
+        #self.aimstation.addItems(['大连', '西安'])
+
+    def searchdetail(self):
+        pass
+        #cur.execute("select * from searchdetail()")
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    loginui = Login()
+    conductorui = Conductor()
+    loginui = Login(conductorui)
     loginui.show()
     sys.exit(app.exec())
